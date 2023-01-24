178ca819912c3953f4d67ff7a40dab47875b2b15
LeifAG <leif.hammar@gymnasium.ax>
2023-01-24 15:00:32 +0200

    hosts

--- listsite/settings.py
+++ listsite/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-)9!%@7ow_-1wd-&0$8-on$^!j!p@)ew=0ahujtlht5gu01z_xz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-2043.up.railway.app','127.0.0.1','localhost:8000','localhost']
-CSRF_TRUSTED_ORIGINS = ['https://web-production-2043.up.railway.app']
+ALLOWED_HOSTS = ['listsitepgd-production.up.railway.app','127.0.0.1','localhost:8000','localhost']
+CSRF_TRUSTED_ORIGINS = ['https://listsitepgd-production.up.railway.app']
 
 
 
